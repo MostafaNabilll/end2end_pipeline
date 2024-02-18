fcbbe7a156e67f4a7c588723c5d2c8544ee7904e
Mostafa Nabil <60539423+MostafaNabilll@users.noreply.github.com>
2024-02-18 18:50:47 +0100

    Add Airflow dag

--- airflow/dags/data_orchestration.py
+++ airflow/dags/data_orchestration.py
@@ -1,9 +1,22 @@
 from airflow import DAG
-from airflow.operators.sensors import S3KeySensor
-from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.hooks.base_hook import BaseHook
 from datetime import datetime, timedelta
+import os
 
+current_date = datetime.utcnow().strftime('%Y%m%d%H')
+dag_directory = os.path.dirname(os.path.realpath(__file__))
+script_path = os.path.join(dag_directory, '../src/data_generator/denormalized_data.py')
+
+s3_bucket_name = 'dogspipeline-personal'
+s3_key_pattern = f'data/output_data_{current_date}.csv'
+snowflake_table = "full_data"
+snowflake_schema = 'DOGS'
+snowflake_conn_id = 'snowflake_conn'
+snowflake_stage = 's3_stage'
 
 default_args = {
     'owner': 'airflow',
@@ -13,40 +26,101 @@ default_args = {
 }
 
 dag = DAG(
-    'data_orchestration',
+    dag_id='dog_data_generation',
     default_args=default_args,
-    schedule_interval='@daily',  # Adjust the schedule as needed
+    schedule_interval='@daily',
     catchup=False
 )
 
-s3_sensor_task = S3KeySensor(
-    task_id='wait_for_s3_file',
-    bucket_name='dogspipeline-personal',
-    bucket_key='s3://dogspipeline-personal/data/',
-    timeout=6000,
-    poke_interval=60,
-    mode='poke',
+def run_denormalized_data_script(**kwargs):
+    aws_conn_id = 'x'
+    aws_hook = BaseHook.get_hook(aws_conn_id)
+    
+    aws_access_key_id = aws_hook.get_connection(aws_conn_id).login
+    aws_secret_access_key = aws_hook.get_connection(aws_conn_id).password
+
+    os.environ['AWS_ACCESS_KEY_ID'] = aws_access_key_id
+    os.environ['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
+
+    os.system(f'python {script_path}')
+
+def check_s3_file(**kwargs):
+    try:
+        s3_hook = S3Hook(aws_conn_id='aws_conn')
+        bucket_name = 'dogspipeline-personal'
+        s3_key = f'data/output_data_{current_date}.csv'
+
+        # Use list_keys() to get a list of keys in the bucket
+        keys = s3_hook.list_keys(bucket_name=bucket_name)
+
+        print("List of S3 keys:")
+        for key in keys:
+            print(key.encode('utf-8'))
+
+        # Check if the specific key exists
+        if s3_key in keys:
+            print('File added to S3')
+            print(s3_key)
+            kwargs['ti'].xcom_push(key='s3_key', value=s3_key)
+            return s3_key
+        else:
+            print('No file has been added')
+            return None
+    except Exception as e:
+        print(f'Error checking S3: {str(e)}')
+        return None
+
+
+run_denormalized_data_task = PythonOperator(
+    task_id='run_denormalized_data',
+    python_callable=run_denormalized_data_script,
+    provide_context=True,
     dag=dag,
 )
 
-# Task 2: Update Tables in Snowflake
-snowflake_update_task = S3ToSnowflakeOperator(
-    task_id='update_snowflake_tables',
-    sql='sql_script.sql',
-    snowflake_conn_id='snowflake_conn',
-    autocommit=True,
-    warehouse='compute_wh',
-    database='dogpipeline',
-    schema='dogs',
+check_s3_objects_task = PythonOperator(
+    task_id='check_s3_files',
+    python_callable=check_s3_file,
+    provide_context=True,
     dag=dag,
 )
 
-# Task 3: Run dbt Tasks
-dbt_task = BashOperator(
-    task_id='run_dbt_tasks',
-    bash_command='dbt run',
+insert_into_table_task = SnowflakeOperator(
+    task_id='insert_into_table',
+    snowflake_conn_id=snowflake_conn_id,
+    sql="""
+        COPY INTO DOGS.stg_data
+        FROM @S3_STAGE/
+        FILE_FORMAT = (TYPE = 'CSV', FIELD_OPTIONALLY_ENCLOSED_BY = '"', FIELD_DELIMITER = ';', COMPRESSION = 'NONE',   ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE);
+    """,
     dag=dag,
 )
 
-# Set up task dependencies
-s3_sensor_task >> snowflake_update_task >> dbt_task
+PATH_TO_DBT_PROJECT = os.path.join(dag_directory, '../dbt/dogspipeline')
+DBT_PROFILES_DIR = os.path.join(dag_directory, '../dbt/dogspipeline/profiles.yml')
+
+    
+initate_dbt_task = BashOperator(
+    task_id = 'dbt_initiate',
+    bash_command = 'dbt deps && dbt seed --select state_codes --profiles-dir . --target dev',
+    cwd=PATH_TO_DBT_PROJECT,
+    dag=dag,
+)
+
+execute_dbt_task = BashOperator(
+    task_id = 'dbt_run',
+    bash_command = 'dbt deps && dbt run --profiles-dir . --target dev',
+    cwd=PATH_TO_DBT_PROJECT,
+    dag=dag,
+)
+
+test_dbt_data_task = BashOperator(
+    task_id='dbt_test',
+    bash_command='dbt test --profiles-dir . --target dev',
+    cwd=PATH_TO_DBT_PROJECT,
+    dag=dag,
+)
+
+
+check_s3_objects_task >> insert_into_table_task
+run_denormalized_data_task >> check_s3_objects_task >> insert_into_table_task >> initate_dbt_task >> execute_dbt_task >> test_dbt_data_task
--- airflow/sql/sql_script.sql
+++
@@ -1,3 +0,0 @@
-COPY INTO "DOGPIPELINE"."DOGS"."full_data"
-FROM @s3_stage/denormalized_data.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"');
