2af90bb1bb767c033713b89cb356f3db1c1e1ef0
Mostafa Nabil <60539423+MostafaNabilll@users.noreply.github.com>
2024-02-18 18:51:12 +0100

    Update DBT models

--- dbt/dogspipeline/models/intermediate/inter_orders.sql
+++ dbt/dogspipeline/models/intermediate/inter_orders.sql
@@ -3,32 +3,52 @@
     unique_key= 'transaction_id'
 ) }}
 
-SELECT
-    fl.transaction_id,
-    fl.order_id,
-    fl.order_date,
-    do.owner_id,
-    dd.dog_id,
-    dv.vet_id,
-    da.appointment_id,
-    dm.product_id,
-    fl.quantity,
-    fl.subtotal,
-    dm.product_price
+with dedup as (
+    SELECT
+        fl.transaction_id,
+        fl.order_id,
+        fl.order_date,
+        do.owner_id,
+        dd.dog_id,
+        dv.vet_id,
+        da.appointment_id,
+        dm.product_id,
+        fl.quantity,
+        fl.subtotal,
+        dm.product_price,
+        ROW_NUMBER() OVER (PARTITION BY transaction_id ORDER BY order_date) AS row_num
+    FROM
+        {{ref('inter_data')}} AS fl
+    LEFT JOIN 
+        {{ref('inter_owner')}} AS do 
+            ON fl.owner_id = do.owner_id
+    LEFT JOIN 
+        {{ref('inter_dog')}} AS dd 
+            ON fl.dog_id = dd.dog_id
+    LEFT JOIN 
+        {{ref('inter_veterinarian')}} AS dv 
+            ON fl.veterinarian_id = dv.vet_id
+    LEFT JOIN 
+        {{ref('inter_appointment')}} AS da 
+            ON fl.appointment_id = da.appointment_id
+    LEFT JOIN 
+        {{ref('inter_product')}} AS dm 
+            ON fl.product_id = dm.product_id
+)
+
+SELECT 
+    transaction_id,
+    order_id,
+    order_date,
+    owner_id,
+    dog_id,
+    vet_id,
+    appointment_id,
+    product_id,
+    quantity,
+    subtotal,
+    product_price
 FROM
-    {{ref('inter_data')}} AS fl
-LEFT JOIN 
-    {{ref('inter_owner')}} AS do 
-        ON fl.owner_id = do.owner_id
-LEFT JOIN 
-    {{ref('inter_dog')}} AS dd 
-        ON fl.dog_id = dd.dog_id
-LEFT JOIN 
-    {{ref('inter_veterinarian')}} AS dv 
-        ON fl.veterinarian_id = dv.vet_id
-LEFT JOIN 
-    {{ref('inter_appointment')}} AS da 
-        ON fl.appointment_id = da.appointment_id
-LEFT JOIN 
-    {{ref('inter_product')}} AS dm 
-        ON fl.product_id = dm.product_id
+    dedup
+WHERE
+    row_num = 1
